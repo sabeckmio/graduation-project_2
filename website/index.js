1fef4a7c3394949f8723bab06c4f815375a3b930
cedar0930 <jonna8282>
2024-03-21 18:55:27 +0900

    포스트맨&Nodemon&회원가입

--- website/index.js
+++ website/index.js
@@ -1,19 +1,35 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const config = require("./config/key");
+
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 const mongoose = require("mongoose");
+
 mongoose
-  .connect(
-    "mongodb+srv://dainguk:Nso3B7e1Re46TBQQ@chatbot.hc69olx.mongodb.net/?retryWrites=true&w=majority&appName=chatbot"
-  )
+  .connect(config.mongoURI)
   .then(() => console.log("mongodb connected"))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("테스트");
 });
 
-// 주석
+app.post("/register", (req, res) => {
+  // 회원 가입 할때 필요한 정보를 클라이언트에서 가져오면
+  // 데이터베이스 넣음
+
+  const user = new User(req.body);
+  user
+    .save()
+    .then(() => res.status(200).json({ success: true }))
+    .catch((err) => res.json({ success: false, err }));
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
